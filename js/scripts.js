0c0e211ef74679e72325345a6cb16339531c23d4
Qianqian Hu <qianhu@pindous.com>
2017-11-07 15:02:41 -0800

    add some styles on css

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ function Pizza(inputSize, inputTopping) {
   this.size = inputSize;
   this.topping = inputTopping;
   this.price = function() {
-    var basicPrice = 3;
+
+    var basicPrice = 3.99;
 
     if (this.size === "extra") {
       basicPrice += 2;
@@ -11,26 +12,21 @@ function Pizza(inputSize, inputTopping) {
     if (this.size === "large" || this.size === "medium") {
       basicPrice += 1;
     }
-    if (this.topping === "cheese") {
+    if (this.topping.includes("cheese")) {
       basicPrice += 2;
     }
-    if (this.topping === "pepperoni") {
+    if (this.topping.includes("pepperoni")) {
       basicPrice += 1;
     }
-    if (this.topping === "artichoke") {
+    if (this.topping.includes("artichoke")) {
       basicPrice += 2;
     }
-    if (this.topping === "anchovy") {
+    if (this.topping.includes("anchovy")) {
       basicPrice += 1;
     }
     return basicPrice;
   };
 }
-/*
-1. for each add pizza button click -> create a Pizza instance
-2. push every pizza instance to pizzas array (global variable)
-3. once you click "place your order", you iterate through [Pizza instance].
-*/
 
 function resetResultsFields() {
   $("#pizza").text("");
@@ -40,13 +36,11 @@ function resetResultsFields() {
 // user interface logic
 $(document).ready(function() {
 
-  //global variable to contain all pizza instances
   var pizzas = [];
 
   $("#add-pizza").click(function() {
-
+    $(".boxmodel").show();
     var pizzaSize = $("#size").val();
-
     var pizzaToppings = [];
     $('input[type="checkbox"]:checked').each(function(index, elem) {
         pizzaToppings.push($(elem).val());
@@ -61,29 +55,16 @@ $(document).ready(function() {
        newPizzaContents += "Pizza " + (i+1).toString() + "<br><br>";
        newPizzaContents += "Size: " + pizzas[i].size + "<br>";
        newPizzaContents += "Toppings: " +  pizzas[i].topping.toString() + "<br>";
-       newPizzaContents += "Price: " + pizzas[i].price() + "<br><br>";
+       newPizzaContents += "Price: " + pizzas[i].price() + " dollars." + "<br><br>";
        totalPrice += pizzas[i].price();
        $("#pizza").append(newPizzaContents);
     }
-
-    //$("#totalPrice").text(totalPrice.toString());
-
   });
 
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
 
     resetResultsFields();
-
-    /*
-    ("#pizza").show();
-    $("#size").text(pizzaSize);
-    $("input:checkbox[name=topping]:checked").each(function() {
-      var toppingSelected = $(this).val();
-      $("#topping").append(toppingSelected + ", ");
-    });
-    $("#price").text(price);
-    */
 
     var totalPrice = 0.0;
 
@@ -92,14 +73,12 @@ $(document).ready(function() {
        newPizzaContents += "Pizza " + (i+1).toString() + "<br><br>";
        newPizzaContents += "Size: " + pizzas[i].size + "<br>";
        newPizzaContents += "Toppings: " +  pizzas[i].topping.toString() + "<br>";
-       newPizzaContents += "Price: " + pizzas[i].price() + "<br><br>";
+       newPizzaContents += "Price: " + pizzas[i].price() + " dollars." +"<br><br>";
        totalPrice += pizzas[i].price();
        $("#pizza").append(newPizzaContents);
     }
 
     $("#words").show();
-    $("#totalPrice").text(totalPrice.toString());
-    //resetFields();
-
+    $("#totalPrice").text(totalPrice.toString()+ " dollars.");
   });
 });
